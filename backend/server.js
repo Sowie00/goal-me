e75df1e9e6b8a8d53ce7f32d333418a45e8e67e3
Sourav Pandya <pandyasourav38@gmail.com>
2022-06-09 02:24:29 -0400

    Initial REST API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const colors = require('colors');
+const connectDB = require('./config/db')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000;
+connectDB()
 const app = express();
 const { errorHandler } = require('./middleware/errorMiddleware')
 
